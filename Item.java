437a358ab442d0bbef98fce4e785c5eb88fc714b
Maximillian Moore <mooremaximillian@gmail.com>
2017-02-08 12:19:12 -0500

    Adds the item's class methods as well as documentation

--- Item.java
+++ Item.java
@@ -1,9 +1,27 @@
+/**
+ * This Class describes an items weight and name
+ */
 public class Item
 {
+  // name of item
   String name;
-  
+  // weight of item
   int weight;
-  
-  
+  // Accesor method to get the Name
+  public String getName()
+  {
+    return name;
+  }
+  // Accessor method to get the weight
+  public int getWeight()
+  {
+    return weight;
+  }
+  // This method prints a description with name and weight
+  public void examine()
+  {
+   System.out.println("This object's name is " + name + ", and it's weight is " + weight + ",");
+                        
+  }
   
 }
