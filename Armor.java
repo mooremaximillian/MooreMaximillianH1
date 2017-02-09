aae3ec357ed72bfd98c0a597bf2989fe19085743
Maximillian Moore <mooremaximillian@gmail.com>
2017-02-09 15:03:48 -0500

    Documents the Armor class

--- Armor.java
+++ Armor.java
@@ -1,7 +1,11 @@
+/**
+ * This class describes an Armor item
+ */ 
 public class Armor
 {
+  //Declares an int defense
   private int defense;
- 
+ //Accesor method to get defense.
   public int getDefense()
   {
     return defense;
